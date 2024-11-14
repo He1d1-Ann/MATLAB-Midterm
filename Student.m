354ab7e3f1ed99dd70d040d981ed944360c3f711
He1d1-Ann <160255431+He1d1-Ann@users.noreply.github.com>
2024-11-13 21:46:00 -0500

    Update Student.m

--- Student.m
+++ Student.m
@@ -6,7 +6,7 @@
 % Date: 11/11/2024
 
 classdef Student
-    #Defines properties of the class
+    %Defines properties of the class
     properties
         ID
         Name
@@ -16,7 +16,7 @@ classdef Student
     end
     
     methods
-    #Makes these variables into objects
+    %Makes these variables into objects
         function obj = Student(id, name, age, gpa, major)
             obj.ID = id;
             obj.Name = name;
@@ -24,12 +24,12 @@ classdef Student
             obj.GPA = gpa;
             obj.Major = major;
         end
-        #Displays the objects
+        %Displays the objects
         function displayInfo(obj)
             fprintf('ID: %s\nName: %s\nAge: %d\nGPA: %.2f\nMajor: %s\n', ...
                 obj.ID, obj.Name, obj.Age, obj.GPA, obj.Major);
         end
-        #Allows GPA to be updated
+        %Allows GPA to be updated
         function obj = updateGPA(obj, newGPA)
             obj.GPA = newGPA;
         end
